dad922abd349abeba1aad9bf388c0ed5048123bb
rlaalsrud0 <kmk001203@gmail.com>
2021-07-15 16:47:40 +0900

    gui 계산기 2단계 구현
    숫자키 동작하도록 변경
    백스페이스 구현 완료

---
+++ calc-gui.js
@@ -0,0 +1,41 @@
+var input = {'array' : []};
+
+input.getInput = function(){
+    return this.array.join("");
+}
+
+var output = {};
+output.text = document.getElementById('output');
+
+var clickNumbers = function(event){
+    var str = event.target.innerHTML;
+    console.log(str);
+    switch(str){
+        case 'BS' :
+        input.array.pop();
+        break;
+        case '+' :
+        input.array.push(' ' + str + ' ');
+        break;
+        case '-' :
+        input.array.push(' ' + str + ' ');
+        break;
+        case '*' :
+        input.array.push(' ' + str + ' ');
+        break;
+        case '/' :
+        input.array.push(' ' + str + ' ');
+        break;
+        default :
+        input.array.push(str);
+    }
+    if(input.array.length === 0){
+        output.text.innerHTML = "Empty";
+    }else{
+        output.text.innerHTML = input.getInput();
+    }
+}
+var showResult = function(event){
+    console.log("click others");
+    console.log(event.target.innerHTML);
+}
