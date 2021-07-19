afb99c1966cc0b9f82b6f2db2fcc10291e6f5d14
rlaalsrud0 <kmk001203@gmail.com>
2021-07-19 10:41:02 +0900

    [15puzzle] 진행 상황 처리
    한 문제를 맞추면 다음 문제를 출제
    한 문제를 맞출 때마다 상단에 O를 하나씩 추가
    세 문제를 맞추면 Thank you for playing! 문구 표시

--- 15-puzzle/word.js
+++ 15-puzzle/word.js
@@ -1,72 +1,137 @@
-var word1 = document.getElementById('word1');
-var word2 = document.getElementById('word2');
-var check = document.getElementById('check');
+// html elements
+var word1 = document.getElementById('word1');  //answer
+var word2 = document.getElementById('word2');  //buttons
+var check = document.getElementById('check');  //word1 === word2?
+var progress = document.getElementById('progress');  //progress check
 
-var words = 'apple,linux,javascript,tutorial'.split(',');
-
-var game = {};
-game.choice = function(){
-    var idx = Math.floor(Math.random() * words.length);
-    return words[idx];
+//game objects
+var game = { 
+    'btns': [],
+    'maxPlay' : 3,
+    'current' : 0
 };
-var answer = game.choice();
-word1.innerHTML = answer;
 
-game.word = answer.split('');
-game.btns = [];
+game.words = 'apple,linux,javascript,tutorial'.split(',');
 
-game.updateDisplay = function(){
-    if(answer === game.word.join('')){
+//choose 1 word from words
+game.choose = function () {
+    var idx = Math.floor(Math.random() * this.words.length);
+    this.answer = this.words[idx];
+    this.letters = this.answer.split('');
+    word1.innerHTML = this.answer;
+};
+
+game.addButtons = function () {
+    for (var i = 0; i < this.letters.length; i++) {
+        var btn = document.createElement('button');
+        btn.innerHTML = this.letters[i];
+        word2.appendChild(btn);
+        this.btns.push(btn);
+    }
+};
+
+game.removeButtons = function(){
+    for(var i = 0; i < this.btns.length; i++){
+        word2.removeChild(this.btns[i]);
+    }
+    this.btns = [];
+};
+
+game.checkGood = function(){
+    return this.answer === this.letters.join('');
+};
+
+game.updateDisplay = function () {
+    if (this.checkGood()) {
         check.innerHTML = '일치합니다.';
-    }else{
+    } else {
         check.innerHTML = '일치하지 않습니다.';
     }
 };
 
-for(var i = 0; i < answer.length; i++){
-    var btn = document.createElement('button');
-    btn.innerHTML = answer[i];
-    word2.appendChild(btn);
-    game.btns.push(btn);
-}
+game.init = function () {
+    this.choose();
+    this.addButtons();
+    this.updateDisplay();
+};
 
-var swap = function(event){
+game.init();
+
+game.copyBtnText = function () {
+    for (var i = 0; i < this.letters.length; i++) {
+        this.btns[i].innerHTML = this.letters[i];
+    }
+};
+
+game.swap = function(){
     var temp = [];
-    while(game.word.length != 0){
-        var s = game.word.pop();
+    while (game.letters.length != 0) {
+        var s = game.letters.pop();
         temp.push(s);
     }
-    game.word = temp;
+    game.letters = temp;
     game.copyBtnText();
     game.updateDisplay();
 };
 
-game.copyBtnText = function(){
-    for(var i = 0; i < this.word.length; i++){
-        this.btns[i].innerHTML = this.word[i];
+game.rshift = function(){
+    var s = game.letters.pop();
+    game.letters.unshift(s);
+    game.copyBtnText();
+    game.updateDisplay();
+};
+
+game.lshift = function(){
+    var s = game.letters.shift();
+    game.letters.push(s);
+    game.copyBtnText();
+    game.updateDisplay();
+};
+
+game.progress = function(){
+    if(game.checkGood()){
+        game.current++;
+        game.removeButtons();
+        game.init();
+        game.shuffle();
+        var str = "";
+        for(var i = 0; i < game.current; i++){
+            str += "O";
+        }
+        progress.innerHTML = str;
     }
-}
 
-var rshift = function(event){
-    var s = game.word.pop();
-    game.word.unshift(s);
-    game.copyBtnText();
+    if(game.current === game.maxPlay){
+        alert("Good! Thank you for playing");
+    }
 };
 
-var lshift = function(event){
-    var s = game.word.shift();
-    game.word.push(s);
-    game.copyBtnText();
+//event handler for swap button
+var swap = function () {
+    game.swap();
+    game.progress();
 };
 
-var toggle = Math.floor(Math.random() * 2) === 0;
+var rshift = function () {
+    game.rshift();
+    game.progress();
+};
 
-if(toggle){
-    swap();
-}
+var lshift = function () {
+    game.lshift();
+    game.progress();
+};
 
-var n = Math.floor(Math.random() * answer.length);
+//shuffle
+game.shuffle = function () {
+    var toggle = Math.floor(Math.random() * 2) === 0;
+    if (toggle) {
+        game.swap();
+    }
 
-for(var i = 0; i < n; i++){
-    rshift();
-}
+    var n = Math.floor(Math.random() * (game.answer.length - 1));
+    for (var i = 0; i < n; i++) {
+        game.rshift();
+    }
+};
+game.shuffle();
